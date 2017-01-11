c9fa32c563a4d886e24944f58313aad712e07d0f
jason <jsnklpn@gmail.com>
2017-01-11 17:07:14 -0600

    initial add of EF and ASP.net Identity

---
+++ JatanWebApp/Models/DAL/ApplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace JatanWebApp.Models.DAL
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+
+    /// <summary>
+    /// User database model.
+    /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
---
+++ JatanWebApp/Models/DAL/JatanDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace JatanWebApp.Models.DAL
+{
+    public class JatanDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public JatanDbContext()
+            : base("JatanDbContext", throwIfV1Schema: false)
+        {
+        }
+
+        public static JatanDbContext Create()
+        {
+            return new JatanDbContext();
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // The User/Identity DbSet (tables) properties are added automatically by IdentityDbContext.
+    }
+}
